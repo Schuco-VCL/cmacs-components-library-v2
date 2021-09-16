763bddae5c3ca23566b056d51829c8246b0851cf
Malini Yarla <51411815+malini1993@users.noreply.github.com>
2021-09-16 15:42:25 -0400

    PTB-8125

--- lib/components/cmacs-date-picker/date-picker.component.d.ts
+++ lib/components/cmacs-date-picker/date-picker.component.d.ts
@@ -38,7 +38,6 @@ export declare class CmacsDatePickerComponent implements OnInit, OnChanges, OnDe
     autoFocus: boolean;
     disabled: boolean;
     borderless: boolean;
-    showWeekCustom: boolean;
     inputReadOnly: boolean;
     nzOpen?: boolean;
     cmacsOpen?: boolean;
@@ -61,6 +60,7 @@ export declare class CmacsDatePickerComponent implements OnInit, OnChanges, OnDe
     suffixIcon: string | TemplateRef<NzSafeAny>;
     nzId: string | null;
     width: any;
+    private _showWeekCustom;
     readonly onPanelChange: EventEmitter<string | string[] | NzDateMode[]>;
     readonly onCalendarChange: EventEmitter<Date[]>;
     readonly onOk: EventEmitter<CompatibleDate>;
@@ -71,6 +71,8 @@ export declare class CmacsDatePickerComponent implements OnInit, OnChanges, OnDe
     openPickerRightRangeSub: string;
     picker: CmacsPickerComponent;
     get showTime(): SupportTimeOptions | boolean;
+    set showWeekCustom(value: boolean);
+    get showWeekCustom(): boolean;
     set showTime(value: SupportTimeOptions | boolean);
     parseDate(): string | any[];
     constructor(nzConfigService: NzConfigService, datePickerService: DatePickerService, i18n: NzI18nService, cdr: ChangeDetectorRef, renderer: Renderer2, elementRef: ElementRef, dateHelper: DateHelperService, directionality: Directionality, noAnimation?: NzNoAnimationDirective);
@@ -100,6 +102,6 @@ export declare class CmacsDatePickerComponent implements OnInit, OnChanges, OnDe
     nzOnCalendarChange(value: CompatibleValue): void;
     onResultOk(): void;
     static ɵfac: i0.ɵɵFactoryDef<CmacsDatePickerComponent, [null, null, null, null, null, null, null, { optional: true; }, { optional: true; host: true; }]>;
-    static ɵcmp: i0.ɵɵComponentDefWithMeta<CmacsDatePickerComponent, "cmacs-date-picker,cmacs-week-picker,cmacs-month-picker,cmacs-year-picker,cmacs-range-picker", ["cmacsDatePicker"], { "allowClear": "allowClear"; "timeline": "timeline"; "autoFocus": "autoFocus"; "disabled": "disabled"; "borderless": "borderless"; "showWeekCustom": "showWeekCustom"; "inputReadOnly": "inputReadOnly"; "nzOpen": "nzOpen"; "cmacsOpen": "cmacsOpen"; "disabledDate": "disabledDate"; "locale": "locale"; "placeHolder": "placeHolder"; "popupStyle": "popupStyle"; "dropdownClassName": "dropdownClassName"; "size": "size"; "format": "format"; "dateRender": "dateRender"; "disabledTime": "disabledTime"; "nzRenderExtraFooter": "nzRenderExtraFooter"; "showToday": "showToday"; "mode": "mode"; "showNow": "showNow"; "ranges": "ranges"; "defaultPickerValue": "defaultPickerValue"; "separator": "separator"; "suffixIcon": "suffixIcon"; "nzId": "nzId"; "width": "width"; "openPickerTitle": "openPickerTitle"; "openPickerSubtitle": "openPickerSubtitle"; "openPickerLeftRangeSub": "openPickerLeftRangeSub"; "openPickerRightRangeSub": "openPickerRightRangeSub"; "showTime": "showTime"; }, { "onPanelChange": "onPanelChange"; "onCalendarChange": "onCalendarChange"; "onOk": "onOk"; "onOpenChange": "onOpenChange"; }, never, never>;
+    static ɵcmp: i0.ɵɵComponentDefWithMeta<CmacsDatePickerComponent, "cmacs-date-picker,cmacs-week-picker,cmacs-month-picker,cmacs-year-picker,cmacs-range-picker", ["cmacsDatePicker"], { "allowClear": "allowClear"; "timeline": "timeline"; "autoFocus": "autoFocus"; "disabled": "disabled"; "borderless": "borderless"; "inputReadOnly": "inputReadOnly"; "nzOpen": "nzOpen"; "cmacsOpen": "cmacsOpen"; "disabledDate": "disabledDate"; "locale": "locale"; "placeHolder": "placeHolder"; "popupStyle": "popupStyle"; "dropdownClassName": "dropdownClassName"; "size": "size"; "format": "format"; "dateRender": "dateRender"; "disabledTime": "disabledTime"; "nzRenderExtraFooter": "nzRenderExtraFooter"; "showToday": "showToday"; "mode": "mode"; "showNow": "showNow"; "ranges": "ranges"; "defaultPickerValue": "defaultPickerValue"; "separator": "separator"; "suffixIcon": "suffixIcon"; "nzId": "nzId"; "width": "width"; "openPickerTitle": "openPickerTitle"; "openPickerSubtitle": "openPickerSubtitle"; "openPickerLeftRangeSub": "openPickerLeftRangeSub"; "openPickerRightRangeSub": "openPickerRightRangeSub"; "showTime": "showTime"; "showWeekCustom": "showWeekCustom"; }, { "onPanelChange": "onPanelChange"; "onCalendarChange": "onCalendarChange"; "onOk": "onOk"; "onOpenChange": "onOpenChange"; }, never, never>;
 }
 //# sourceMappingURL=date-picker.component.d.ts.map
--- lib/components/cmacs-date-picker/date-range-popup.component.d.ts
+++ lib/components/cmacs-date-picker/date-range-popup.component.d.ts
@@ -13,7 +13,6 @@ export declare class DateRangePopupComponent implements OnInit, OnChanges, OnDes
     isRange: boolean;
     showWeek: boolean;
     timeline: boolean;
-    showWeekCustom: boolean;
     locale: NzCalendarI18nInterface | undefined;
     disabledDate?: DisabledDateFn;
     disabledTime?: DisabledTimeFn;
@@ -35,6 +34,9 @@ export declare class DateRangePopupComponent implements OnInit, OnChanges, OnDes
     hoverValue: SingleValue[];
     checkedPartArr: boolean[];
     destroy$: Subject<unknown>;
+    private _showWeekCustom;
+    set showWeekCustom(value: boolean);
+    get showWeekCustom(): boolean;
     get hasTimePicker(): boolean;
     get hasFooter(): boolean;
     constructor(datePickerService: DatePickerService, cdr: ChangeDetectorRef);
@@ -75,6 +77,6 @@ export declare class DateRangePopupComponent implements OnInit, OnChanges, OnDes
     private overrideTimeOptions;
     private overrideHms;
     static ɵfac: i0.ɵɵFactoryDef<DateRangePopupComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDefWithMeta<DateRangePopupComponent, "date-range-popup", ["dateRangePopup"], { "isRange": "isRange"; "showWeek": "showWeek"; "timeline": "timeline"; "showWeekCustom": "showWeekCustom"; "locale": "locale"; "disabledDate": "disabledDate"; "disabledTime": "disabledTime"; "showToday": "showToday"; "showNow": "showNow"; "showTime": "showTime"; "extraFooter": "extraFooter"; "ranges": "ranges"; "dateRender": "dateRender"; "panelMode": "panelMode"; "defaultPickerValue": "defaultPickerValue"; "dir": "dir"; }, { "panelModeChange": "panelModeChange"; "calendarChange": "calendarChange"; "resultOk": "resultOk"; }, never, never>;
+    static ɵcmp: i0.ɵɵComponentDefWithMeta<DateRangePopupComponent, "date-range-popup", ["dateRangePopup"], { "isRange": "isRange"; "showWeek": "showWeek"; "timeline": "timeline"; "locale": "locale"; "disabledDate": "disabledDate"; "disabledTime": "disabledTime"; "showToday": "showToday"; "showNow": "showNow"; "showTime": "showTime"; "extraFooter": "extraFooter"; "ranges": "ranges"; "dateRender": "dateRender"; "panelMode": "panelMode"; "defaultPickerValue": "defaultPickerValue"; "dir": "dir"; "showWeekCustom": "showWeekCustom"; }, { "panelModeChange": "panelModeChange"; "calendarChange": "calendarChange"; "resultOk": "resultOk"; }, never, never>;
 }
 //# sourceMappingURL=date-range-popup.component.d.ts.map
